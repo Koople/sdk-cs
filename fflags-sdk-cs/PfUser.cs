d70dc5595b94c74329f5db2f1a8b4268ecd5288d
Oscar GV <galindero@gmail.com>
2020-10-12 12:33:15 +0200

    create PfUser

---
+++ fflags-sdk-cs/PfUser.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using fflags_sdk_cs.Values;
+
+namespace fflags_sdk_cs
+{
+    public class PfUserAttribute {
+        public readonly string Name;
+        public readonly object Value;
+
+        public PfUserAttribute(string name, object value)
+        {
+            Name = name;
+            Value = value;
+        }
+    }
+    
+    public class PfUser
+    {
+        public readonly string Identity;
+        public readonly Dictionary<string, PfValue<object>> Attributes;
+    }
+}
